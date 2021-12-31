79d568f76bfb7ba7753ad4881b14f0a508aaaeb4
= <=>
2021-12-31 15:30:35 +0530

    update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
 mod print_blockchain;
 mod mine;
 
-use std::env;
-use std::fs;
+use std::io;
 
 fn main() {
-    println!("Hello there!\nWhat do you want to do?\n1. Print blockchain\n2. Start mining\n3. Reset blockchain\n4. Quit");
-    print_blockchain::print_blockchain();
+    println!("Hello there! What do you want to do?\n1. Print blockchain\n2. Start mining\n3. Reset blockchain\n4. Quit");
+    
+    loop {
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                if input.trim() == "1" {
+                    println!("Printing Blockchain");
+                }
+                else if input.trim() == "2" {
+                    println!("Starting mining");
+                }
+                else if input.trim() == "3" {
+                    println!("Resetting the ledger");
+                }
+                else if input.trim() =="4" {
+                    return;
+                }
+                else {
+                    println!("Enter a valid option");
+                }
+            }
+            Err(e) => {
+            println!("Something went wrong, here is the error message:\n{}",e);
+            break;
+            }
+        }
+    }
 
 }
