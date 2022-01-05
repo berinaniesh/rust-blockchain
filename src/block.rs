3cfa3cd529cb88fbe8796a4492100b8379c6585a
= <=>
2022-01-05 14:51:30 +0530

    update

--- src/block.rs
+++ src/block.rs
@@ -1,3 +1,5 @@
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 use sha2::{Sha256, Digest};
 struct Block {
     block_number: u64,
@@ -5,20 +7,31 @@ struct Block {
     previous_block_hash: String,
 }
 impl Block {
-    pub fn block_to_string() -> String {
-       block_number.to_string() + block_data + previous_block_hash
-    }
-    pub fn hash_block() -> String {
-        let mut hash = Sha256::new();
-        hash.finalize_fixed(block_to_string())
+    pub fn block_to_string(&self) -> String {
+       let mut ans: String = String::new();
+       ans.push_str(self.block_number.to_string().as_str());
+       ans.push_str(self.block_data.as_str());
+       ans.push_str(self.previous_block_hash.as_str());
+       ans
     }
 }
 
-pub fn testing() {
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+        
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+
+}
+
+fn testing() {
+
     let block_1 = Block {
         block_number: 1,
         block_data: String::from("First block"),
         previous_block_hash: String::from("0")
     };
-    println!()
-} 
+
+    println!("{}", calculate_hash(&block_1))
+    
+}
